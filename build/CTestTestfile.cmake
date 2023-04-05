da648444ee6efd11b35e55336613519d875014a5
Cynthia <fadzai@student.chalmers.se>
2023-04-05 09:44:31 +0000

    Deleted CMakeLists.txt, build/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c, build/CMakeFiles/3.22.1/CompilerIdC/a.out, build/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp, build/CMakeFiles/3.22.1/CompilerIdCXX/a.out, build/CMakeFiles/3.22.1/CMakeCCompiler.cmake, build/CMakeFiles/3.22.1/CMakeCXXCompiler.cmake, build/CMakeFiles/3.22.1/CMakeDetermineCompilerABI_C.bin, build/CMakeFiles/3.22.1/CMakeDetermineCompilerABI_CXX.bin, build/CMakeFiles/3.22.1/CMakeSystem.cmake, build/CMakeFiles/helloworld-Runner.dir/DependInfo.cmake, build/CMakeFiles/helloworld-Runner.dir/PrimeChecker.cpp.o, build/CMakeFiles/helloworld-Runner.dir/PrimeChecker.cpp.o.d, build/CMakeFiles/helloworld-Runner.dir/TestPrimeChecker.cpp.o, build/CMakeFiles/helloworld-Runner.dir/TestPrimeChecker.cpp.o.d, build/CMakeFiles/helloworld-Runner.dir/build.make, build/CMakeFiles/helloworld-Runner.dir/cmake_clean.cmake, build/CMakeFiles/helloworld-Runner.dir/compiler_depend.make, build/CMakeFiles/helloworld-Runner.dir/compiler_depend.ts, build/CMakeFiles/helloworld-Runner.dir/depend.make, build/CMakeFiles/helloworld-Runner.dir/flags.make, build/CMakeFiles/helloworld-Runner.dir/link.txt, build/CMakeFiles/helloworld-Runner.dir/progress.make, build/CMakeFiles/helloworld.dir/DependInfo.cmake, build/CMakeFiles/helloworld.dir/PrimeChecker.cpp.o, build/CMakeFiles/helloworld.dir/PrimeChecker.cpp.o.d, build/CMakeFiles/helloworld.dir/build.make, build/CMakeFiles/helloworld.dir/cmake_clean.cmake, build/CMakeFiles/helloworld.dir/compiler_depend.make, build/CMakeFiles/helloworld.dir/compiler_depend.ts, build/CMakeFiles/helloworld.dir/depend.make, build/CMakeFiles/helloworld.dir/flags.make, build/CMakeFiles/helloworld.dir/helloworld.cpp.o, build/CMakeFiles/helloworld.dir/helloworld.cpp.o.d, build/CMakeFiles/helloworld.dir/link.txt, build/CMakeFiles/helloworld.dir/progress.make, build/CMakeFiles/CMakeDirectoryInformation.cmake, build/CMakeFiles/CMakeOutput.log, build/CMakeFiles/Makefile.cmake, build/CMakeFiles/Makefile2, build/CMakeFiles/TargetDirectories.txt, build/CMakeFiles/cmake.check_cache, build/CMakeFiles/progress.marks, build/Testing/Temporary/CTestCostData.txt, build/Testing/Temporary/LastTest.log, build/CMakeCache.txt, build/CTestTestfile.cmake, build/Makefile, build/cmake_install.cmake, build/helloworld, build/helloworld-Runner, .vscode/c_cpp_properties.json, .vscode/launch.json, .vscode/settings.json

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/akuien/projects/2023-group-09
-# Build directory: /home/akuien/projects/2023-group-09/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(helloworld-Runner "/home/akuien/projects/2023-group-09/build/helloworld-Runner")
-set_tests_properties(helloworld-Runner PROPERTIES  _BACKTRACE_TRIPLES "/home/akuien/projects/2023-group-09/CMakeLists.txt;8;add_test;/home/akuien/projects/2023-group-09/CMakeLists.txt;0;")
